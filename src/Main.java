3d9337bf8ea7f13daebcdd1a04881dca54be2ad9
Olga Norik <norinaoa@mail.ru>
2020-02-20 15:23:04 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Стоимость билета Price в копейках
+        long Price = 2_500_000;
+        long miles = Price / 20 / 100;
+        System.out.println(miles);
+    }
+}
